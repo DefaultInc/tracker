408594474c497909a22121e2c0decce1ead8ec7d
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2017-12-08 16:53:24 +0600

    transactions finished

--- tracker/settings.py
+++ tracker/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
 
-from AWS.envparser import get
+from aws.envparser import get
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
